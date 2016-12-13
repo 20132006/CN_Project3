4012979ef9dfd181ce478e21cfed235eaec3e703
20132006 <taalaibekuulualibek@gmail.com>
2016-12-13 20:39:56 +0900

    files
    adding files

---
+++ client.c
@@ -0,0 +1,631 @@
+/* tcpclient.c */
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <<time.h>>
+
+double last_update=0.0;
+bool first_update = true;
+char *required_filer;
+int required_IP[4];
+int sock;
+char send_data[2048];
+char recv_data[1024];
+
+int index;
+pthread_mutex_t sendReceiveMutex;
+
+//Linked list implementation
+
+struct node {
+   int data;
+
+   int src1;
+   int src2;
+   int src3;
+   int src4;
+
+   int dst1;
+   int dst2;
+   int dst3;
+   int dst4;
+
+   struct node *next;
+};
+
+struct node *head = NULL;
+struct node *current = NULL;
+
+void storeData(int num)
+{
+    int k=0;
+    char temp[30];
+    while (num>0)
+    {
+        temp[++k] = num%10;
+        num/=10;
+    }
+    --k;
+    for (;k>=0;k--)
+    {
+        send_data[++index] = temp[k];
+    }
+}
+
+//display the list
+void sendList() {
+
+    index = 0;
+    //memseit(src, '\0',sizeof(src));
+    strcpy(src,inet_ntoa(iph->ip_src));
+    struct node *ptr = head;
+
+    memset(send_data, 0, sizeof (send_data));;
+    //start from the beginning
+    while(ptr != NULL)
+    {
+        pthread_mutex_lock(&sendReceiveMutex);/*                                  lock and check requred_filer*/
+        if (strcmp(required_filer , "filter:all") == 0)
+        {
+            storeData(ptr->src1);send_data[++index] = '.';
+            storeData(ptr->src2);send_data[++index] = '.';
+            storeData(ptr->src3);send_data[++index] = '.';
+            storeData(ptr->src4);send_data[++index] = '-';send_data[++index] = '>';
+            storeData(ptr->dst1);send_data[++index] = '.';
+            storeData(ptr->dst2);send_data[++index] = '.';
+            storeData(ptr->dst3);send_data[++index] = '.';
+            storeData(ptr->dst4);send_data[++index] = ':';
+            storeData(ptr->data);send_data[++index] = '\n';
+        }
+        else if (strcmp(required_filer , "filter:star") == 0 &&
+        (ptr->src1 == required_IP[0] && ptr->src3 == required_IP[1] && ptr->src3 == required_IP[2]) ||
+        (ptr->dst1 == required_IP[0] && ptr->dst2 == required_IP[1] && ptr->dst3 == required_IP[2]) )
+        {
+            storeData(ptr->src1);send_data[++index] = '.';
+            storeData(ptr->src2);send_data[++index] = '.';
+            storeData(ptr->src3);send_data[++index] = '.';
+            storeData(ptr->src4);send_data[++index] = '-';send_data[++index] = '>';
+            storeData(ptr->dst1);send_data[++index] = '.';
+            storeData(ptr->dst2);send_data[++index] = '.';
+            storeData(ptr->dst3);send_data[++index] = '.';
+            storeData(ptr->dst4);send_data[++index] = ':';
+            storeData(ptr->data);send_data[++index] = '\n';
+        }
+        else if (strcmp(required_filer , "filter:full") == 0 &&
+        (ptr->src1 == required_IP[0] && ptr->src3 == required_IP[1] &&
+          ptr->src3 == required_IP[2] && ptr->src4 == required_IP[3]) ||
+        (ptr->dst1 == required_IP[0] && ptr->dst2 == required_IP[1] &&
+          ptr->dst3 == required_IP[2] && ptr->dst4 == required_IP[3]) )
+        {
+            storeData(ptr->src1);send_data[++index] = '.';
+            storeData(ptr->src2);send_data[++index] = '.';
+            storeData(ptr->src3);send_data[++index] = '.';
+            storeData(ptr->src4);send_data[++index] = '-';send_data[++index] = '>';
+            storeData(ptr->dst1);send_data[++index] = '.';
+            storeData(ptr->dst2);send_data[++index] = '.';
+            storeData(ptr->dst3);send_data[++index] = '.';
+            storeData(ptr->dst4);send_data[++index] = ':';
+            storeData(ptr->data);send_data[++index] = '\n';
+        }
+        pthread_mutex_unlock(&sendReceiveMutex);/*                                  lock and check requred_filer*/
+        printf("(%d.%d.%d.%d) ->",ptr->src1,ptr->src2,ptr->src3,ptr->src4);
+        printf("(%d.%d.%d.%d) : %d\n",ptr->dst1,ptr->dst2,ptr->dst3,ptr->dst4,ptr->data);
+        ptr = ptr->next;
+    }
+    send(sock,send_data,strlen(send_data), 0);
+
+}
+
+//insert link at the first location
+void insertFirst(int src1, int src2, int src3, int src4,
+                 int dst1, int dst2, int dst3, int dst4, int data) {
+   //create a link
+   struct node *link = (struct node*) malloc(sizeof(struct node));
+
+   link->src1 = src1;
+   link->src2 = src2;
+   link->src3 = src3;
+   link->src4 = src4;
+
+   link->dst1 = dst1;
+   link->dst2 = dst2;
+   link->dst3 = dst3;
+   link->dst4 = dst4;
+
+   link->data = data;
+
+   //point it to old first node
+   link->next = head;
+
+   //point first to new first node
+   head = link;
+}
+
+//delete first item
+struct node* deleteFirst() {
+
+   //save reference to first link
+   struct node *tempLink = head;
+
+   //mark next to first link as first
+   head = head->next;
+
+   //return the deleted link
+   return tempLink;
+}
+
+//is list empty
+bool isEmpty() {
+   return head == NULL;
+}
+
+int length() {
+   int length = 0;
+   struct node *current;
+
+   for(current = head; current != NULL; current = current->next) {
+      length++;
+   }
+
+   return length;
+}
+
+//find a link with given key
+struct node* find(int src1, int src2, int src3, int src4,
+                    int dst1, int dst2, int dst3, int dst4) {
+
+   //start from the first link
+   struct node* current = head;
+
+   //if list is empty
+   if(head == NULL) {
+      return NULL;
+   }
+
+   //navigate through list
+   while(current->src1 != src1 && current->src2 != src2 && current->src3 != src3 && current->src4 != src4
+      && current->dst1 != dst1 && current->dst2 != dst2 && current->dst3 != dst3 && current->dst4 != dst4) {
+
+      //if it is last node
+      if(current->next == NULL) {
+         return NULL;
+      } else {
+         //go to next link
+         current = current->next;
+      }
+   }
+
+   //if data found, return the current Link
+   return current;
+}
+
+
+//delete a link with given key
+struct node* delete(int src1, int src2, int src3, int src4,
+                    int dst1, int dst2, int dst3, int dst4) {
+
+   //start from the first link
+   struct node* current = head;
+   struct node* previous = NULL;
+
+   //if list is empty
+   if(head == NULL) {
+      return NULL;
+   }
+
+   //navigate through list
+   while(current->src1 != src1 && current->src2 != src2 && current->src3 != src3 && current->src4 != src4
+      && current->dst1 != dst1 && current->dst2 != dst2 && current->dst3 != dst3 && current->dst4 != dst4) {
+
+      //if it is last node
+      if(current->next == NULL) {
+         return NULL;
+      } else {
+         //store reference to current link
+         previous = current;
+         //move to next link
+         current = current->next;
+      }
+   }
+
+   //found a match, update the link
+   if(current == head) {
+      //change first to point to next link
+      head = head->next;
+   } else {
+      //bypass the current link
+      previous->next = current->next;
+   }
+
+   return current;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#define SNAP_LEN 1518
+
+truct arg_struct{
+    //passing argument struct
+    int numberOfPackets;
+    char *filtering_rule;
+    char *hostIP;
+    int Sport;
+};
+
+//Callback function is called when the packet passes through the filter.
+void callback(u_char *useless, const struct pcap_pkthdr *pkthdr, const u_char *packet)
+{
+    static int count = 1;
+    struct ether_header *ep;
+    unsigned short ether_type;
+    int chcnt =0;
+    int length=pkthdr->len;
+
+    //Get the ethernet header.
+    ep = (struct ether_header *)packet;
+    //In order to get IP header, offset a size of ethernet header.
+    packet += sizeof(struct ether_header);
+    //Get a protocol type.
+    ether_type = ntohs(ep->ether_type);
+
+    int fd;
+    struct ifreq ifr;
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    /* I want to get an IPv4 IP address */
+    ifr.ifr_addr.sa_family = AF_INET;
+    /* I want IP address attached to "eth0" */
+    strncpy(ifr.ifr_name, "wlan0", IFNAMSIZ-1);
+    ioctl(fd, SIOCGIFADDR, &ifr);
+    close(fd);
+
+    struct ip *iph = (struct ip *)packet;
+    char *src;
+    src = (char *)malloc(40*sizeof(char));
+    //memseit(src, '\0',sizeof(src));
+    strcpy(src,inet_ntoa(iph->ip_src));
+    //printf("%s\n",src);
+    char *dest;
+    dest = (char *)malloc(40*sizeof(char));
+    //memseit(src, '\0',sizeof(src));
+    strcpy(dest,inet_ntoa(iph->ip_dst));
+    //printf("%s\n",src);
+    char *ipaddr;
+    ipaddr = (char *)malloc(40*sizeof(char));
+    strcpy(ipaddr,inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+    int res = strcmp(ipaddr,src);
+    //printf("%d\n", res);
+    int res1 = strcmp(ipaddr,dest);
+    //printf("%d\n", res);
+
+    int i;
+    int len;
+    int src1[4];
+    int src2[4];
+    int k=0;
+    len = strlen(src);
+    src1[0]=src1[1]=src1[2]=src1[3]=0;
+    for (i=0;i<len;++i)
+    {
+        if (src[i] == '.')
+        {
+            k++;
+        }
+        else
+        {
+            src1[k] = (src1[k] * 10) + (src[i]-48);
+        }
+    }
+
+    k=0;
+    len = strlen(dest);
+    src2[0]=src2[1]=src2[2]=src2[3]=0;
+    for (i=0;i<len;++i)
+    {
+        if (dest[i] == '.')
+        {
+            k++;
+        }
+        else
+        {
+            src2[k] = (src2[k] * 10) + (dest[i]-48);
+        }
+    }
+
+    double clock_time = clock();
+    if (first_update)
+    {
+        last_update = clock_time
+    }
+    else if (!first_update)
+    {
+        double time_spent = (double)(clock_time - last_update) / CLOCKS_PER_SEC;
+
+        if (time_spent > 60)
+        {
+            sendList();
+            while (!isEmpty())
+            {
+                deleteFirst();
+            }
+            last_update = clock_time;
+        }
+
+    }
+    node *temp;
+    temp = find(src1[0],src1[1],src1[2],src1[3],src2[0],src2[1],src2[2],src2[3]);
+    if (temp != NULL)
+    {
+        temp->data++;
+    }
+    else
+    {
+        insertFirst(src1[0],src1[1],src1[2],src1[3],src2[0],src2[1],src2[2],src2[3],0);
+    }
+    if (!res || !res1)
+    {
+        printf("Version     : %u\n", iph->ip_v );
+        printf("Header Len  : %u\n", iph->ip_hl);
+        printf("Ident       : %d\n", iph->ip_id);
+        printf("TTL         : %u\n", iph->ip_ttl);
+        printf("Src Address : %s\n", inet_ntoa(iph->ip_src));
+    	  printf("Dst Address : %s\n", inet_ntoa(iph->ip_dst));
+        printf("\n\n");
+    }
+}
+
+
+
+
+
+
+
+
+
+void *pcap(void *arguments)
+{
+    struct arg_struct *argum;
+    argum  = (struct arg_struct *) arguments;
+
+    char *dev = NULL;
+    bpf_u_int32 net;
+    bpf_u_int32 mask;
+
+    bpf_u_int32 netp;
+    bpf_u_int32 maskp;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    int ret;
+    struct pcap_pkthdr hdr;
+    struct in_addr net_addr, mask_addr;
+    struct ether_header *eptr;
+    const u_char *packet;
+    struct bpf_program fp;
+
+    pcap_t *pcd;
+  //added by Alibek
+    int num_packets = atoi(argv[1]);			/* number of packets to capture */
+    char *filter_exp = argv[2];		/* filter expression [3] */
+    pcap_t *handle;				/* packet capture handle */
+
+
+    //Get a current device name.
+    dev = pcap_lookupdev(errbuf);
+    //printf("Stoped here %s\n", dev);
+    if (dev == NULL)
+    {
+        printf("%s\n", errbuf);
+        exit(1);
+    }
+    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1)
+    {
+        printf("Couldn't get netmask for device %s: %s\n",dev, errbuf);
+        net = 0;
+        mask = 0;
+    }
+    /* open capture device */
+    handle = pcap_open_live(dev, SNAP_LEN, 1, 1000, errbuf);
+    if (handle == NULL)
+    {
+        printf("Couldn't open device %s: %s\n", dev, errbuf);
+        exit(1);
+    }
+    /* make sure we're capturing on an Ethernet device [2] */
+    if (pcap_datalink(handle) != DLT_EN10MB)
+    {
+        printf("%s is not an Ethernet\n", dev);
+        exit(1);
+    }
+    /* compile the filter expression */
+    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1)
+    {
+        printf("Couldn't parse filter %s: %s\n",filter_exp, pcap_geterr(handle));
+        exit(1);
+    }
+    /* apply the compiled filter */
+    if (pcap_setfilter(handle, &fp) == -1)
+    {
+        printf("Couldn't install filter %s: %s\n",filter_exp, pcap_geterr(handle));
+        exit(1);
+    }
+    /* now we can set our callback function */
+    pcap_loop(handle, num_packets, callback, NULL);
+    /* cleanup */
+    pcap_freecode(&fp);
+    pcap_close(handle);
+    printf("\nCapture complete.\n");
+    return 0;
+}
+
+
+
+
+
+
+
+void get_requirements()
+{
+    int len = strlen(recv_data);
+    int i=0;
+    while (recv_data[i]!=':')
+    {
+        i++;
+    }
+    i++;
+    int k;
+    for (k=0;k<3;++k)
+    {
+        required_IP[k]=0;
+        while (recv_data[i]!='.')
+        {
+            required_IP[k] = required_IP[k]*10 + (recv_data[i]-48);
+            ++i;
+        }
+    }
+    ++i;
+    if (recv_data[i]=='*')
+    {
+        strcpy(requred_filer,"filter:star");
+    }
+    else
+    {
+        strcpy(requred_filer,"filter:full");
+        required_IP[k]=0;
+        while (i<len)
+        {
+            required_IP[k] = required_IP[k]*10 + (recv_data[i]-48);
+            ++i;
+        }
+    }
+}
+
+void *connection_handler(void *unused)
+{
+    int bytes_recieved;
+
+    while(1)
+    {
+        bytes_recieved=recv(sock,recv_data,1024,0);
+        recv_data[bytes_recieved] = '\0';
+
+        if (strcmp(recv_data , "q") == 0 || strcmp(recv_data , "Q") == 0)
+        {
+            close(sock);
+            break;
+        }
+
+        else if (bytes_recieved != 0)
+        {
+            pthread_mutex_lock(&sendReceiveMutex);/*                                  lock and check requred_filer*/
+            get_requirements();
+            pthread_mutex_lock(&sendReceiveMutex);/*                                  lock and check requred_filer*/
+            printf("\nRecieved data = %s " , recv_data);
+        }
+        /*
+        printf("\nSEND (q or Q to quit) : ");
+        gets(send_data);
+
+        if (strcmp(send_data , "q") != 0 && strcmp(send_data , "Q") != 0)
+            send(sock,send_data,strlen(send_data), 0);
+
+        else
+        {
+            send(sock,send_data,strlen(send_data), 0);
+            close(sock);
+            break;
+        }
+        */
+    }
+}
+
+int main()
+{
+    int bytes_recieved;
+    char recv_data[1024];
+    struct hostent *host;
+    struct sockaddr_in server_addr;
+    pthread_t thread_id;
+
+    host = gethostbyname("127.0.0.1");
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("Socket");
+        exit(1);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(5000);
+    server_addr.sin_addr = *((struct in_addr *)host->h_addr);
+    bzero(&(server_addr.sin_zero),8);
+
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1)
+    {
+        perror("Connect");
+        exit(1);
+    }
+    if( pthread_create( &thread_id , NULL ,  connection_handler , NULL) < 0)
+    {
+        perror("could not create thread");
+        return 1;
+    }
+    required_filer = (char *)malloc(40*sizeof(char));
+    //memseit(src, '\0',sizeof(src));
+    strcpy(required_filer,"filter:all");
+
+    if (pthread_mutex_init(&sendReceiveMutex, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        return 1;
+    }
+    /*
+    while(1)
+    {
+        bytes_recieved=recv(sock,recv_data,1024,0);
+        recv_data[bytes_recieved] = '\0';
+
+        if (strcmp(recv_data , "q") == 0 || strcmp(recv_data , "Q") == 0)
+        {
+            close(sock);
+            break;
+        }
+
+        else
+            printf("\nRecieved data = %s " , recv_data);
+
+        printf("\nSEND (q or Q to quit) : ");
+        gets(send_data);
+
+        if (strcmp(send_data , "q") != 0 && strcmp(send_data , "Q") != 0)
+            send(sock,send_data,strlen(send_data), 0);
+
+        else
+        {
+            send(sock,send_data,strlen(send_data), 0);
+            close(sock);
+            break;
+        }
+    }
+    */
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,118 @@
+/* Creates a datagram server. The port number is passed as an argument. This server runs forever */
+/* tcpserver.c */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+int sock, connected;
+int bytes_recieved , true_ = 1;
+char send_data [1024] , recv_data[2048];
+/*
+ * This will handle connection for each client
+ * */
+void *connection_handler(void *unused)
+{
+    //Get the socket descriptor
+    while (1)
+    {
+        bytes_recieved = recv(connected,recv_data,2048,0);
+
+        recv_data[bytes_recieved] = '\0';
+
+        if (strcmp(recv_data , "q") == 0 || strcmp(recv_data , "Q") == 0)
+        {
+            close(connected);
+            break;
+        }
+
+        else
+        {
+            printf("---------------------------------------------------------\n", );
+            printf("%s",recv_data);
+            printf("---------------------------------------------------------\n\n", );
+        }
+    }
+    fflush(stdout);
+    return 0;
+}
+
+int main()
+{
+
+
+    struct sockaddr_in server_addr,client_addr;
+    int sin_size;
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("Socket");
+        exit(1);
+    }
+
+    if (setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,&true_,sizeof(int)) == -1)
+    {
+        perror("Setsockopt");
+        exit(1);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(5000);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    bzero(&(server_addr.sin_zero),8);
+
+    if (bind(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1)
+    {
+        perror("Unable to bind");
+        exit(1);
+    }
+
+    if (listen(sock, 5) == -1)
+    {
+        perror("Listen");
+        exit(1);
+    }
+
+    printf("\nTCPServer Waiting for client on port 5000");
+    fflush(stdout);
+
+
+    while(1)
+    {
+        sin_size = sizeof(struct sockaddr_in);
+        connected = accept(sock, (struct sockaddr *)&client_addr,&sin_size);
+        printf("\n I got a connection from (%s , %d)",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+
+        if( pthread_create( &thread_id , NULL ,  connection_handler , NULL) < 0)
+        {
+          perror("could not create thread");
+          return 1;
+        }
+        while (1)
+        {
+            printf("\n SEND (q or Q to quit) : ");
+            gets(send_data);
+
+            if (strcmp(send_data , "q") == 0 || strcmp(send_data , "Q") == 0)
+            {
+                send(connected, send_data,strlen(send_data), 0);
+                close(connected);
+                break;
+            }
+
+            else
+               send(connected, send_data,strlen(send_data), 0);
+
+            fflush(stdout);
+        }
+    }
+
+    close(sock);
+    return 0;
+}
